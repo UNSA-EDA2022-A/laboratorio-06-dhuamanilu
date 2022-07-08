7b43e367b7f81b0e663eb488db003f4552a5c761
dhuamanilu <87215556+dhuamanilu@users.noreply.github.com>
2022-07-08 13:23:22 -0500

    Resolviendo ejercicio 2

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -21,7 +21,18 @@ public class Exercise2 {
 
 
     public <T extends Comparable<T>> boolean bstIguales(BST<T> a1, BST<T> a2){
-
-        return false;
+    	//Se empieza a analizar desde la raiz de ambos árboles
+        return bstIguales(a1.root,a2.root);
+    }
+    public <T extends Comparable<T>> boolean bstIguales(Node<T> a1, Node<T> a2){
+    	//Reutilizemos el código del ejercicio 1
+    	//Si ambos estan vacios,son iguales
+    	if(a1==null && a2==null) 
+    		return true;
+    	//Si uno de los nodos es nulo y el otro tiene algun valor significa que no son iguales
+    	else if(a1!=null && a2==null || (a1==null && a2!=null)) 
+    		return false;
+    	//Sino entonces verificamos la igualdad para sus subárboles izquierdos  y derechos 
+        return a1.data.compareTo(a2.data)==0 && bstIguales(a1.left,a2.left) && bstIguales(a1.right,a2.right);
     }
 }
