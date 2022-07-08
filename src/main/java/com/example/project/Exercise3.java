9a713d5e2d4e8227e41ff253646c1bf35ca4b8b0
dhuamanilu <87215556+dhuamanilu@users.noreply.github.com>
2022-07-08 13:44:02 -0500

    Resolviendo ejercicio 3

--- src/main/java/com/example/project/Exercise3.java
+++ src/main/java/com/example/project/Exercise3.java
@@ -19,7 +19,17 @@ public class Exercise3 {
 
 
     public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
-
-        return false;
+    	//Misma lógica usada en los anteriores ejercicios,empezamos el metodo recursivo desde la raiz
+        return bstEstrictamenteBinario(a.root);
+    }
+    public <T extends Comparable<T>> boolean bstEstrictamenteBinario(Node<T> a){
+    	//Si el nodo actual esta vacio,entonces es estrictamente binario
+    	if(a==null) 
+    		return true;
+    	//Sino entonces los nodos hijos del nodo actual tienen que tener la misma "paridad" , ambos tienen que ser null
+    	//o ambos diferente de null,es decir,no hay ningun nodo con 1 solo hijo
+    	boolean c1= (a.left==null && a.right==null);
+    	boolean c2= (a.left!=null && a.right!=null);
+    	return  (c1 || c2) && bstEstrictamenteBinario(a.left) && bstEstrictamenteBinario(a.right);
     }
 }
