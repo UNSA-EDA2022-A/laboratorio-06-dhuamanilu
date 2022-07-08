c6ee069bce11bca100af8391a1475e932ae6e4e4
dhuamanilu <87215556+dhuamanilu@users.noreply.github.com>
2022-07-08 13:09:33 -0500

    Resolviendo ejercicio 1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -21,7 +21,21 @@ public class Exercise1 {
 
 
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
-
-        return false;
+    	//Dos ABB son SIMILARES si tienen la misma distribución de nodos
+    	//(independientemente de sus valores)
+    	//utilizamos la raiz de ambos arboles en el comienzo del metodo recursivo
+    	return bstSimilares(a1.root,a2.root);
+    }
+    public <T extends Comparable<T>> boolean bstSimilares(Node<T> a1, Node<T> a2){
+    	//En una definición más formal, dos ABB son
+    	//SIMILARES si ambos están vacíos
+    	if(a1==null && a2==null) 
+    		return true;
+    	//Si uno de los nodos es nulo y el otro tiene algun valor significa que no son similares
+    	else if(a1!=null && a2==null || (a1==null && a2!=null)) 
+    		return false;
+    	//Si sus subárboles izquierdos son similares y
+    	//sus subárboles derechos también son similares.
+        return bstSimilares(a1.left,a2.left) && bstSimilares(a1.right,a2.right);
     }
 }
